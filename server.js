49afa30cf266672eb5dd81c28f2b557164d3cba7
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-10-20 23:04:08 -0400

    Adding basic Express JS file. Listens on port and then checks if the key given is in the links as a key to the (key,url) pairs. It then redirects to the url corresponding to the key.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+let links = {
+	'google':'http://www.google.com',
+	'face':'http://www.facebook.com'
+}
+
+const checkKey = (req,res,next) => {
+	const key = req.params.key;
+	if (key in links) {
+		next();
+	} else {
+		res.status(404).send("Cannot be found.");
+	}
+}
+
+app.get('/:key', checkKey, (req,res,next) => {
+	res.redirect(links[req.params.key]);
+});
+
+app.listen(port, () => console.log(`Example app listen on port ${port}!`));
