191242b3dc99ee176a23e9041499123d019cf8c4
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-11-22 02:53:14 -0500

    Adding ESLint and custom linting rules to project (was set up through answering questions from eslint --init command).

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
-let links = {
-	'google':'http://www.google.com',
-	'face':'http://www.facebook.com'
-}
+const links = {
+    google: 'http://www.google.com',
+    face: 'http://www.facebook.com'
+};
 
-const checkKey = (req,res,next) => {
-	const key = req.params.key;
-	if (key in links) {
-		next();
-	} else {
-		res.status(404).send("Cannot be found.");
-	}
-}
+const checkKey = (req, res, next) => {
+    const key  = req.params.key;
+    if (key in links) {
+        next();
+    } else {
+        res.status(404).send('Cannot be found.');
+    }
+};
 
-app.get('/:key', checkKey, (req,res,next) => {
-	res.redirect(links[req.params.key]);
+app.get('/:key', checkKey, (req, res, next) => {
+    res.redirect(links[req.params.key]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
