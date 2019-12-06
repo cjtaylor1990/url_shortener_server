6515b7c9466cb78a24f89ef1feb942c94e1aac0f
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-12-06 00:48:22 -0500

    Edited main server.js file to include a generalized key checking middleware function. I also added in the basic HTTP PUT, POST, and DELETE function. I've not tested these yet.

--- src/server.js
+++ src/server.js
@@ -7,17 +7,46 @@ const links = {
     face: 'http://www.facebook.com'
 };
 
-const checkKey = (req, res, next) => {
-    const key  = req.params.key;
-    if (key in links) {
-        next();
+const findKeyCheckCondition = (method) => {
+    if (method in ['GET', 'PUT', 'DELETE']) {
+        return (key, links) => (key in links);
     } else {
-        res.status(404).send('Cannot be found.');
+        return (key, links) => (!(key in links));
     }
 };
 
+const checkKey = (req, res, next) => {
+    const key  = req.params.key;
+    const keyCheckCondition = findKeyCheckCondition(req.method)(key);
+    if (keyCheckCondition) {
+        next();
+    } else {
+        res.status(404).send();
+    }
+};
+
+app.use(checkKey);
+
 app.get('/:key', checkKey, (req, res, next) => {
-    res.redirect(links[req.params.key]);
+    res.status(200).redirect(links[req.params.key]);
+    next();
+});
+
+app.put('/:key', checkKey, (req, res, next) => {
+    links[req.params.key] = req.query.url;
+    res.status(200).send();
+    next();
+});
+
+app.post('/', checkKey, (req, res, next) => {
+    links[req.params.key] = req.query.url;
+    res.status(201).send();
+    next();
+});
+
+app.delete('/:key', checkKey, (req, res, next) => {
+    delete links[req.params.key];
+    res.status(204).send();
     next();
 });
 
