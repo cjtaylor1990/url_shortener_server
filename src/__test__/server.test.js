a647c652664e46248952fb3081dcddaaa4927103
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-02-17 09:46:31 -0500

    Adding MongoDB Connectivity
    Adding MongoDB connection to config/db.js and the
    URI in a seperate file (config/default.json) listed in the
    .gitignore. I also added in Mongoose and Config to the
    dependency. FInally, I deleted the redundant test/ as it's
    been replaced by src/__test__

--- src/__test__/server.test.js
+++ src/__test__/server.test.js
@@ -1,4 +1,3 @@
-
 describe('Sample test', () => {
     it('should test that true === true', () => {
         expect(true).toBe(true);
--- src/server.js
+++ src/server.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const keyChecker = require('./keyChecker.js');
-const links = require('./links.js');
+const express = require("express");
+const keyChecker = require("./keyChecker.js");
+const links = require("./links.js");
 const port = 3000;
 const app = express();
 
+const connectDB = require("../config/db.js");
+connectDB();
+
 app.use(keyChecker.checkKey);
 
-app.get('/:key', (req, res, next) => {
-    res.status(200).redirect(links[req.params.key]);
-    next();
+app.get("/:key", (req, res, next) => {
+  res.status(200).redirect(links[req.params.key]);
+  next();
 });
 
-app.put('/:key', (req, res, next) => {
-    links[req.params.key] = req.query.url;
-    res.status(200).send();
-    next();
+app.put("/:key", (req, res, next) => {
+  links[req.params.key] = req.query.url;
+  res.status(200).send();
+  next();
 });
 
-app.post('/', (req, res, next) => {
-    links[req.params.key] = req.query.url;
-    res.status(201).send();
-    next();
+app.post("/", (req, res, next) => {
+  links[req.params.key] = req.query.url;
+  res.status(201).send();
+  next();
 });
 
-app.delete('/:key', (req, res, next) => {
-    delete links[req.params.key];
-    res.status(204).send();
-    next();
+app.delete("/:key", (req, res, next) => {
+  delete links[req.params.key];
+  res.status(204).send();
+  next();
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
